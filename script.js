95238567563762295ecc50c4d36ccb0aa48b195b
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-09 10:10:03 +0200

    styles and audio

--- script.js
+++ script.js
@@ -2,19 +2,25 @@ const word = document.querySelector('.word')
 const def = document.querySelector('.def')
 const search_bar = document.querySelector('.search')
 const search_button = document.querySelector('.search_button')
+const phonetic = document.querySelector('.phonetic')
+const audio = document.querySelector('.audio')
+const play_button = document.querySelector('.play_button')
 
-search_button.addEventListener('click', ()=> {loadData()})
+search_button.addEventListener('click', ()=> {
+    if(!search_bar.value==''){loadData()}
+})
+
+play_button.addEventListener('click', ()=>{audio.play()})
 
 async function loadData() {
     const response = await fetch('https://api.dictionaryapi.dev/api/v2/entries/en/'+search_bar.value);
     const data = await response.json();
-    another(data)
+    search(data)
   }
-  loadData();
 
-  function another(data){
-      console.log(data[0].meanings[0].definitions[0].definition)
-
-      word.innerHTML = data[0].word
-      def.innerHTML = data[0].meanings[0].definitions[0].definition
-  }
+function search(data){
+    word.innerHTML = data[0].word
+    phonetic.innerHTML = data[0].phonetics[0].text
+    audio.src = data[0].phonetics[1].audio
+    def.innerHTML = data[0].meanings[0].definitions[0].definition
+}
