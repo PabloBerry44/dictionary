9485cf5eb2faf597166fac40e8f892844e16db71
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-09 13:15:45 +0200

    output more definitions for one part of speech

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const wordTag = document.querySelector('.word')
-const def = document.querySelector('.def')
 const search_bar = document.querySelector('.search')
 const search_button = document.querySelector('.search_button')
 const phoneticTag = document.querySelector('.phonetic')
 const audioTag = document.querySelector('.audio')
 const play_button = document.querySelector('.play_button')
+const definitionsTag = document.querySelector('.definitionsTag')
+
+
 
 search_button.addEventListener('click', ()=> {
     if(!search_bar.value==''){loadData()}
@@ -24,9 +26,9 @@ async function loadData() {
   }
 
 function search(data){
+
     let audioUrl = ''
     let phoneticText = ''
-
     // go through all phonetics indexes
     for(i=0; i<data[0].phonetics.length; i++){
         
@@ -48,10 +50,42 @@ function search(data){
             phoneticTag.style.display = 'none'
         }
     }
+
+    while(definitionsTag.firstChild){
+        definitionsTag.removeChild(definitionsTag.lastChild);
+      }
+    //grab all definitions
+    for(i=0; i<data[0].meanings.length; i++){
+
+        //create div and add class to it
+        const meaningDiv = document.createElement('div')
+        meaningDiv.classList.add(data[0].meanings[i].partOfSpeech)
+        definitionsTag.appendChild(meaningDiv)
+
+        // create paragraph and insert text into it
+        const partOfSpeech = document.createElement('p')
+        meaningDiv.appendChild(partOfSpeech)
+        partOfSpeech.innerHTML = meaningDiv.className
+
+        let meaning = data[0].meanings[i]
+        //create paragraphs and add definitions to them
+        for(i=0; i<meaning.definitions.length; i++){
+
+            const definitionText = document.createElement('p')
+            meaningDiv.appendChild(definitionText)
+
+            definitionText.innerHTML = meaning.definitions[i].definition + '<br><br>'
+        }
+
+
+
+
+    }
+
+
     wordTag.innerHTML = data[0].word
     phoneticTag.innerHTML = phoneticText
     audioTag.src = audioUrl
-    def.innerHTML = data[0].meanings[0].definitions[0].definition
 
     search_bar.value = ''
 }
