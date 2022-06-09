8749483b3690f3101c5ca0b6f7cb2a0a65f42243
pabloberry44 <pawel.borowiec@tutanota.com>
2022-06-09 14:38:43 +0200

    styles, counter, fixes

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const phoneticTag = document.querySelector('.phonetic')
 const audioTag = document.querySelector('.audio')
 const play_button = document.querySelector('.play_button')
 const definitionsTag = document.querySelector('.definitionsTag')
+const definitionsContainer = document.querySelector('.definitionsContainer')
 
 
 
@@ -26,66 +27,87 @@ async function loadData() {
   }
 
 function search(data){
+    while(definitionsContainer.firstChild){
+        definitionsContainer.removeChild(definitionsContainer.lastChild)
+    }
 
-    let audioUrl = ''
-    let phoneticText = ''
-    // go through all phonetics indexes
-    for(i=0; i<data[0].phonetics.length; i++){
+    for(j=0; j<data.length; j++){
+        currentData = data[j]
+
+        let audioUrl = ''
+        let phoneticText = ''
+        // go through all phonetics indexes
+        for(i=0; i<currentData.phonetics.length; i++){
         
-        //find audioURL
-        if(data[0].phonetics[i].audio && !audioUrl){
-            audioUrl=data[0].phonetics[i].audio
-            play_button.style.display = 'block'
-        }
-        else if(!data[0].phonetics[i].audio && !audioUrl){
-            play_button.style.display = 'none'
+            //find audioURL
+            if(currentData.phonetics[i].audio && !audioUrl){
+                audioUrl=currentData.phonetics[i].audio
+                play_button.style.display = 'block'
+            }
+            else if(!currentData.phonetics[i].audio && !audioUrl){
+                play_button.style.display = 'none'
+            }
+
+            //find phonetics text
+            if(currentData.phonetics[i].text && !phoneticText){
+                phoneticText=currentData.phonetics[i].text
+                phoneticTag.style.display = 'block'
+            }
+            else if(!currentData.phonetics[i].audio && !phoneticText){
+                phoneticTag.style.display = 'none'
+            }
         }
 
-        //find phonetics text
-        if(data[0].phonetics[i].text && !phoneticText){
-            phoneticText=data[0].phonetics[i].text
-            phoneticTag.style.display = 'block'
+
+
+
+
+        let definitionsTag = document.createElement('div')
+        definitionsTag.classList.add('definitionsTag')
+        definitionsContainer.appendChild(definitionsTag)
+
+
+        while(definitionsTag.firstChild){
+            definitionsTag.removeChild(definitionsTag.lastChild)
         }
-        else if(!data[0].phonetics[i].audio && !phoneticText){
-            phoneticTag.style.display = 'none'
+        //grab all definitions
+        for(i=0; i<currentData.meanings.length; i++){
+
+            //create div and add class to it
+            const meaningDiv = document.createElement('div')
+            meaningDiv.classList.add(currentData.meanings[i].partOfSpeech)
+            definitionsTag.appendChild(meaningDiv)
+
+            // create paragraph and insert text into it
+            const partOfSpeech = document.createElement('p')
+            meaningDiv.appendChild(partOfSpeech)
+            partOfSpeech.classList.add('part_of_speech')
+            partOfSpeech.innerHTML = meaningDiv.className
+
+            let meaning = currentData.meanings[i]
+            //create paragraphs and add definitions to them
+            for(i=0; i<meaning.definitions.length; i++){
+
+                const definitionText = document.createElement('p')
+                definitionText.classList.add('definition')
+
+                // const definitionIndex = document.createElement('p')
+                // definitionIndex.classList.add('definition_index')
+
+                // meaningDiv.appendChild(definitionIndex)
+                meaningDiv.appendChild(definitionText)
+                // definitionIndex.innerHTML = i+'.'
+                definitionText.innerHTML = meaning.definitions[i].definition + '<br><br>'
+            }
         }
+
+        wordTag.innerHTML = currentData.word
+        phoneticTag.innerHTML = phoneticText
+        audioTag.src = audioUrl
+
+        search_bar.value = ''
+
     }
 
-    while(definitionsTag.firstChild){
-        definitionsTag.removeChild(definitionsTag.lastChild);
-      }
-    //grab all definitions
-    for(i=0; i<data[0].meanings.length; i++){
-
-        //create div and add class to it
-        const meaningDiv = document.createElement('div')
-        meaningDiv.classList.add(data[0].meanings[i].partOfSpeech)
-        definitionsTag.appendChild(meaningDiv)
-
-        // create paragraph and insert text into it
-        const partOfSpeech = document.createElement('p')
-        meaningDiv.appendChild(partOfSpeech)
-        partOfSpeech.innerHTML = meaningDiv.className
-
-        let meaning = data[0].meanings[i]
-        //create paragraphs and add definitions to them
-        for(i=0; i<meaning.definitions.length; i++){
-
-            const definitionText = document.createElement('p')
-            meaningDiv.appendChild(definitionText)
-
-            definitionText.innerHTML = meaning.definitions[i].definition + '<br><br>'
-        }
-
-
-
-
-    }
-
-
-    wordTag.innerHTML = data[0].word
-    phoneticTag.innerHTML = phoneticText
-    audioTag.src = audioUrl
-
-    search_bar.value = ''
+    
 }
